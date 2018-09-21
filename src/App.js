3cec7adfd94613549708bb6bb587f5ccad6bc0de
Ovidio R. Calvet <calveto@gmail.com>
2018-09-21 18:37:42 -0400

    Add speed and angle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
-import classnames from 'classnames';
 
 class App extends Component {
   state = {
     angle: 0,
+    speed: 1,
     spin: false,
     rotationInterval: undefined
   };
@@ -18,7 +17,7 @@ class App extends Component {
   }
 
   reDraw = (timestamp) => {
-    this.setState({ angle: (this.state.angle > 360) ? 1 : this.state.angle + 1 });
+    this.setState({ angle: (this.state.angle > 360) ? this.state.speed : this.state.angle + this.state.speed });
     if (this.state.spin) {
       requestAnimationFrame(this.reDraw);
     }
@@ -29,13 +28,30 @@ class App extends Component {
     this.draw();
   }
 
+  speedUp = () => {
+    this.setState({ speed: this.state.speed + 5 });
+  }
+
+  slowDown = () => {
+    this.setState({ speed: this.state.speed - 5 < 1 ? 1 : this.state.speed - 5 });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
         <div>
-          <button onClick={this.spin}>Spin</button>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between', width: 200, marginBottom: 10 }}>
+            <button onClick={this.spin}>{this.state.spin ? 'Stop' : 'Spin'}</button>
+            <button onClick={this.speedUp}>Speed Up</button>
+            <button onClick={this.slowDown}>Slow Down</button>
+          </div>
+          <div style={{ textAlign: 'center' }}>
+            Speed: {this.state.speed} | Angle: {this.state.angle}
+          </div>
         </div>
-        <div style={{ backgroundColor: 'red', height: 100, width: 100, transform: `rotate(${this.state.angle}deg)` }}></div>
+        <div style={{ display: 'flex', marginTop: 25 }}>
+          <div style={{ backgroundColor: 'red', height: 100, width: 100, transform: `rotate(${this.state.angle}deg)` }}></div>
+        </div>
       </div>
     );
   }
